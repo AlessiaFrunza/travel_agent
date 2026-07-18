e0c3489a433b2f8d437e8217e557d30428beab30
AlessiaFrunza <alessia.frunza.af@gmail.com>
2026-07-18 17:39:53 +0200

    feat(db): add alembic migrations and password_hash field

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv("../.env")
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
